fcc0c4144f2a07f55ae4bb791fb1faa4a94b804a
JoaoTorpe <victotorpe10@gmail.com>
2023-04-21 18:05:32 -0300

    Agora tem tela de registro

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
 const loginPop = document.getElementById("login");
 const exit = document.getElementById("exit");
+const exitregister = document.getElementById("exitregister")
 const email = document.querySelector("#email")
 const password = document.querySelector("#pass")
-
-
+const registerbtn = document.querySelector("#register")
+const logincontainer = document.querySelector(".container")
+const registercontainer = document.querySelector(".containerRegister")
+const formSubmitBtn = document.querySelector("#frombtn")
+//abre o login e fecha a de registro,evita bugs pois elas n podem estar abertas ao mesmo tempo
 loginPop.addEventListener('click',()=>{
 email.value = " ";
 password.value= "";
 document.querySelector(".container").classList.add("pop")
-
+registercontainer.classList.remove("pop")
 })
 
 
-
+//fecha o login
 exit.addEventListener('click',()=>{
 
-    document.querySelector(".container").classList.remove("pop")
+    logincontainer.classList.remove("pop")
     
     })
+
+//popUP da tela de registro
+    registerbtn.addEventListener('click',()=>{
+        logincontainer.classList.remove("pop")
+        registercontainer.classList.add("pop")
+
+
+
+    })
+
+    //fechar tela de registro
+
+    exitregister.addEventListener('click',()=>{
+        registercontainer.classList.remove("pop")
+        })
+
+        //Submit dos forms
+
+
+        formSubmitBtn.addEventListener('click',()=>{
+
+            logincontainer.classList.remove("pop")
+            registercontainer.classList.remove("pop")
+        })
